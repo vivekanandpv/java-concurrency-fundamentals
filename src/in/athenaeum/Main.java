62df17eed2578a47ccf27daf6a73e1dbcab735db
vivekanandpv <vivekanandpv@outlook.com>
2021-06-09 17:14:43 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -3,7 +3,13 @@ package in.athenaeum;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello, world!");
+	    Runnable runnable = () -> {
+            System.out.println("Executing " + Thread.currentThread().getName());
+        };
+
+	    Thread thread = new Thread(runnable);
+	    thread.setName("Custom Thread");
+
+	    thread.start(); //  thread.run() launches the task in the main thread
     }
 }
