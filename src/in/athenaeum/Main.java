2510c812591f7d95110ba4c69563b0f7f337ae2d
vivekanandpv <vivekanandpv@outlook.com>
2021-06-10 20:53:43 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -1,9 +1,52 @@
 package in.athenaeum;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        executorService.submit(() -> {
+            System.out.println("Running a task 1 on: " + Thread.currentThread().getId());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        executorService.submit(() -> {
+            System.out.println("Running a task 2 on: " + Thread.currentThread().getId());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        executorService.submit(() -> {
+            System.out.println("Running a task 3 on: " + Thread.currentThread().getId());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        executorService.execute(() -> {
+            System.out.println("Running a task 4 on: " + Thread.currentThread().getId());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        executorService.shutdown();
     }
 }
