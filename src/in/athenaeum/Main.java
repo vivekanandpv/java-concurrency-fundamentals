2e73563a7c1462e68b0734e2c5f7c93f5aa7d90f
vivekanandpv <vivekanandpv@outlook.com>
2021-06-11 07:31:03 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -5,33 +5,15 @@ import java.util.concurrent.*;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        //  Single-thread
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        ExecutorService fixedThreadExecutor = Executors.newFixedThreadPool(4);
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
 
-        //  Using Callable, value can be returned. This is wrapped in Future<V>
-        Future<Integer> future = executorService.submit(() -> {
-            Thread.sleep(500);
-            return 500;
-        });
-
-        System.out.println("Return value: " + future.get());    //  future.get() is blocking
-
-        Future<String> result = executorService.submit(() -> {
-            if (true) {
-                throw new RuntimeException("Oops!");
-            }
-
-            return "Hi there!";
-        });
-
-        //  When we use Callable, the exception can be propagated from the thread
-        //  Actual exception is wrapped in ExecutionException
-        try {
-            System.out.println("String result: " + result.get());
-        } catch (ExecutionException e) {
-            System.out.println("Caught exception");
-        }
-
-
-        executorService.shutdown();
+        //  Scheduled Executor
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(4);
+        scheduledExecutorService.schedule(() -> System.out.println("Hi there"), 2000, TimeUnit.MILLISECONDS);
+        scheduledExecutorService.scheduleAtFixedRate(() -> System.out.println("Hi there"), 2000, 5000, TimeUnit.MILLISECONDS);  //  after initial delay, scheduled at every 5000; start of the task delay
+        scheduledExecutorService.scheduleWithFixedDelay(() -> System.out.println("Hi there"), 2000, 3000, TimeUnit.MILLISECONDS);   //  after initial delay, scheduled 3000 after the previous task; end of the task delay
     }
 }
